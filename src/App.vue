879287c9f0d89e60b1992b8b54db4aec883407fd
Zetos Lima <cesar_manoel@hotmail.com>
2018-03-18 20:58:22 -0300

    Hamburger & Toolbar implemented with Navigation

--- src/App.vue
+++ src/App.vue
@@ -2,33 +2,40 @@
   <v-app dark>
     <v-navigation-drawer fixed v-model='sideNav'>
       <v-list>
-        <v-list-tile>
+        <v-list-tile 
+          v-for="item in menuItems" 
+          :key="item.title"
+          router
+          :to="item.link">
           <v-list-tile-action>
-            <v-icon>supervisor_account</v-icon>
+            <v-icon>{{ item.icon }}</v-icon>
           </v-list-tile-action>
-          <v-list-tile-content>CultMeetup</v-list-tile-content>
+          <v-list-tile-content>{{ item.title }}</v-list-tile-content>
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
     <v-toolbar class="primary" >
       <v-toolbar-side-icon @click.native.stop='sideNav = !sideNav'
       class='hidden-sm-and-up' ></v-toolbar-side-icon>
-      <v-toolbar-title>CultMeetup</v-toolbar-title>
+      <v-toolbar-title>
+        <router-link to='/' tag='span' style="cursor: pointer" >CultMeetup</router-link>
+      </v-toolbar-title>
       <v-spacer></v-spacer>  
       <v-toolbar-items class='hidden-xs-only' >
-        <v-btn flat>
-        <v-icon left>supervisor_account</v-icon>
-          Meetups
-        </v-btn>
-        <v-btn flat>
-          New Meetup
-        </v-btn>
-        <v-btn flat>
-          Profile
+        <v-btn 
+          flat 
+          v-for="item in menuItems" 
+          :key="item.title"
+          router
+          :to="item.link">
+        <v-icon left>{{ item.icon }}</v-icon>
+          {{ item.title }}
         </v-btn>
       </v-toolbar-items>
     </v-toolbar>
-    <main></main>
+    <main>
+      <router-view></router-view>
+    </main>
   </v-app>
 </template>
 
@@ -36,7 +43,14 @@
 export default {
   data () {
     return {
-      sideNav: false
+      sideNav: false,
+      menuItems: [
+        { icon: 'supervisor_account', title: 'View Meetups', link: '/' },
+        { icon: 'room', title: 'Organize Meetup', link: '/meetup/new' },
+        { icon: 'person', title: 'Profile', link: '/profile' },
+        { icon: 'face', title: 'Sign up', link: '/signup' },
+        { icon: 'lock_open', title: 'Sign in', link: '/signin' }
+      ]
     }
   },
   name: 'App'
