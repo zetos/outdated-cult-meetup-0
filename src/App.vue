fdf74ab951baec1352730295de811468bdcabdba
Zetos Lima <cesar_manoel@hotmail.com>
2018-03-18 19:25:22 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <v-toolbar > </v-toolbar>
+    <main></main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  name: 'App'
+}
+</script>
