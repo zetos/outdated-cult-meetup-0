ef426fdb0a4c3b90ec15f2a35b2e45349713529e
Zetos Lima <cesar_manoel@hotmail.com>
2018-03-18 22:22:11 -0300

    Home with carousel

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,64 @@
 <template>
-  <div>
-      <h1>Home Page</h1>
-  </div>
+  <v-container>
+    <v-layout row wrap class="mb-2">
+      <v-flex xs12 sm6 class="text-xs-center text-sm-right">
+        <v-btn large router to="/meetups" class="info">Explore Meetups</v-btn>
+      </v-flex>
+      <v-flex xs12 sm6 class="text-xs-center text-sm-left">
+        <v-btn large router to="/meetups/new" class="info">Organize Meetup</v-btn>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap class="mb-2">
+      <v-flex xs12>
+        <v-carousel>
+          <v-carousel-item
+            v-for="meetup in meetups"
+            :src="meetup.imageUrl"
+            :key="meetup.id">
+              <div class="title">
+                {{ meetup.title }}
+              </div>
+            </v-carousel-item>
+        </v-carousel>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap class="mb-2">
+      <v-flex xs12 class="text-xs-center">
+        <p>Join our awesome cults!</p>
+      </v-flex>
+    </v-layout>
+    
+  </v-container>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      meetups: [
+        { imageUrl: 'https://i.kinja-img.com/gawker-media/image/upload/s--z9FtuPyJ--/c_scale,fl_progressive,q_80,w_800/mjtzdwibbf0tvkuj4a77.jpg',
+          id: '312321',
+          title: 'Meetup in Arkham' },
+        { imageUrl: 'https://vignette.wikia.nocookie.net/l5r/images/d/d1/Cultists_of_Ruhmal.jpg',
+          id: '312343',
+          title: 'Meetup in Ruhmal' }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+ .title {
+   position: absolute;
+   bottom: 50px;
+   background-color: rgba(0,0,0,0.5);
+   color: white;
+   font-size: 2em !important;
+   padding: 20px;
+   right: 10%;
+   left: 10%;
+   text-align: center;
+ }
+</style>
+
