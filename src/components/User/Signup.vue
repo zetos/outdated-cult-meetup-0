22b486c2e83dafb3d1b503b2c9a1082bcba94e03
Zetos Lima <cesar_manoel@hotmail.com>
2018-03-24 18:12:59 -0300

    Signup page

--- src/components/User/Signup.vue
+++ src/components/User/Signup.vue
@@ -1,5 +1,79 @@
 <template>
-  <div>
-      <h1>Sign up Page</h1>
-  </div>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card>
+          <v-card-text>
+            <v-container>
+              <form>
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-text-field
+                    name="email"
+                    label="Mail"
+                    id="email"
+                    v-model="email"
+                    type="email"
+                    required></v-text-field>
+                  </v-flex>
+                </v-layout>
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-text-field
+                    name="password"
+                    label="Password"
+                    id="password"
+                    v-model="password"
+                    type="password"
+                    required></v-text-field>
+                  </v-flex>
+                </v-layout>
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-text-field
+                    name="confirmPassword"
+                    label="Confirm Password"
+                    id="confirmPassword"
+                    v-model="confirmPassword"
+                    type="confirmPassword"
+                    :rules="[comparePasswords]"
+                    required
+                    ></v-text-field>
+                  </v-flex>
+                </v-layout>
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-btn type="submit">Sign Up</v-btn>
+                  </v-flex>
+                </v-layout>
+              </form>
+            </v-container>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      email: '',
+      password: '',
+      confirmPassword: ''
+    }
+  },
+  computed: {
+    comparePasswords () {
+      return this.password !== this.confirmPassword ? 'Passwords do not match' : ''
+    }
+  },
+  methods: {
+    onSignup () {
+      console.log({email: this.email, password: this.password, confirmPassword: this.confirmPassword})
+    }
+  }
+}
+</script>
+
