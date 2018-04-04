faf6da26375c38e66d408503ef320679c7d79675
Zetos Lima <cesar_manoel@hotmail.com>
2018-04-04 00:00:54 -0300

    Edit meetup date & time

--- src/main.js
+++ src/main.js
@@ -9,6 +9,8 @@ import { store } from './store'
 import DateFilter from './filters/date'
 import Alert from './components/Shared/Alert.vue'
 import EditMeetupDetailsDialog from './components/Meetup/Edit/EditMeetupDetailsDialog.vue'
+import EditMeetupDateDialog from './components/Meetup/Edit/EditMeetupDateDialog.vue'
+import EditMeetupTimeDialog from './components/Meetup/Edit/EditMeetupTimeDialog.vue'
 
 Vue.use(Vuetify, { theme: {
   primary: colors.red.darken4,
@@ -24,6 +26,8 @@ Vue.config.productionTip = false
 Vue.filter('date', DateFilter)
 Vue.component('app-alert', Alert)
 Vue.component('app-edit-meetup-details-dialog', EditMeetupDetailsDialog)
+Vue.component('app-edit-meetup-date-dialog', EditMeetupDateDialog)
+Vue.component('app-edit-meetup-time-dialog', EditMeetupTimeDialog)
 
 /* eslint-disable no-new */
 new Vue({
