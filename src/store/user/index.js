20eb71c3c9b8014699a3076eb97de8deb36839fa
Zetos Lima <cesar_manoel@hotmail.com>
2018-04-07 16:13:28 -0300

    Spliting store

---
+++ src/store/user/index.js
@@ -0,0 +1,140 @@
+import firebase from 'firebase'
+
+export default {
+  state: {
+    user: null
+  },
+  mutations: {
+    registerUserForMeetup (state, payload) {
+      const id = payload.id
+      if (state.user.registeredMeetups.findIndex(meetup => meetup.id === id) >= 0) {
+        return
+      }
+      state.user.registeredMeetups.push(id)
+      state.user.fbKeys[id] = payload.fbKey
+    },
+    unregisterUserFromMeetup (state, payload) {
+      const registeredMeetups = state.user.registeredMeetups
+      registeredMeetups.splice(registeredMeetups.findIndex(meetup => meetup.id === payload), 1)
+      Reflect.deleteProperty(state.user.fbKeys, payload)
+    },
+    setUser (state, payload) {
+      state.user = payload
+    }
+  },
+  actions: {
+    registerUserForMeetup ({commit, getters}, payload) {
+      commit('setLoading', true)
+      const user = getters.user
+      firebase.database().ref(`/users/${user.id}`).child('/registrations/')
+        .push(payload)
+        .then(response => {
+          commit('setLoading', false)
+          commit('registerUserForMeetup', {id: payload, fbKey: response.key})
+        })
+        .catch(error => {
+          console.error('registerUserForMeetup error:', error)
+          commit('setLoading', false)
+        })
+    },
+    unregisterUserFromMeetup ({commit, getters}, payload) {
+      commit('setLoading', true)
+      const user = getters.user
+      if (!user.fbKeys) {
+        return
+      }
+      const fbKey = user.fbKeys[payload]
+      firebase.database().ref(`/users/${user.id}/registrations/`).child(fbKey).remove()
+      .then(() => {
+        commit('setLoading', false)
+        commit('unregisterUserFromMeetup', payload)
+      })
+      .catch(error => {
+        console.error('unregisterUserFromMeetup', error)
+        commit('setLoading', false)
+      })
+    },
+    signUserUp ({commit}, payload) {
+      commit('setLoading', true)
+      commit('clearError')
+      firebase.auth().createUserWithEmailAndPassword(payload.email, payload.password)
+        .then(
+          user => {
+            commit('setLoading', false)
+            const newUser = {
+              id: user.uid,
+              registeredMeetups: [],
+              fbKeys: {}
+            }
+            commit('setUser', newUser)
+          }
+        )
+        .catch(error => {
+          commit('setLoading', false)
+          commit('setError', error)
+          console.error('signUserUp error:', error)
+        })
+    },
+    signUserIn ({commit}, payload) {
+      commit('setLoading', true)
+      commit('clearError')
+      firebase.auth().signInWithEmailAndPassword(payload.email, payload.password)
+        .then(
+          user => {
+            commit('setLoading', false)
+            const newUser = {
+              id: user.uid,
+              registeredMeetups: [],
+              fbKeys: {}
+            }
+            commit('setUser', newUser)
+          }
+        )
+        .catch(error => {
+          commit('setLoading', false)
+          commit('setError', error)
+          console.error('signUserIn error:', error)
+        })
+    },
+    autoSignIn ({commit}, payload) {
+      commit('setUser', {
+        id: payload.uid,
+        registeredMeetups: [],
+        fbKeys: {}
+      })
+    },
+    fetchUserData ({commit, getters}) {
+      commit('setLoading', true)
+      firebase.database().ref(`users/${getters.user.id}/registrations/`).once('value')
+      .then(response => {
+        const responseValues = response.val()
+        let registeredMeetups = []
+        let swappedPairs = {}
+        for (let key in responseValues) {
+          registeredMeetups.push(responseValues[key])
+          swappedPairs[responseValues[key]] = key
+        }
+        const updatedUser = {
+          id: getters.user.id,
+          registeredMeetups: registeredMeetups,
+          fbKeys: swappedPairs
+        }
+        commit('setLoading', false)
+        commit('setUser', updatedUser)
+      })
+      .catch(error => {
+        console.error('fetchUserData error:', error)
+        commit('setLoading', false)
+      })
+    },
+    logout ({commit}) {
+      firebase.auth().signOut()
+      commit('setUser', null)
+    }
+  },
+  getters: {
+    user (state) {
+      return state.user
+    }
+  }
+}
